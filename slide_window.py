cd7bdbcf51fd4cd5b3319513e32a9bf731b5dd65
Diane Trout <diane@ghic.org>
2014-07-17 15:17:01 -0700

    program that reads a fasta file and outputs a new fasta file that contains a sliding window of sequence

---
+++ slide_window.py
@@ -0,0 +1,50 @@
+"""Read a fasta file and generate a sliding window of sequence to map with
+"""
+from __future__ import print_function
+
+import argparse
+
+def main(cmdline=None):
+    parser = make_parser()
+    args = parser.parse_args(cmdline)
+
+    for filename in args.filenames:
+        with open(filename, 'r') as instream:
+            generate_fasta_windows(instream)
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='+')
+    return parser
+
+def read_fasta(stream):
+    """Read a fasta file returning a sequence of name and sequence.
+    
+    the sequence field will have all whitespace removed.
+    """
+    name = None
+    seq = []
+    for line in stream:
+        if line[0] == '>':
+            # if we've seen a complete sequence yield it
+            if name is not None:
+                yield (name, ''.join(seq))
+                seq = []
+            # save the next name
+            name = line[1:].rstrip()
+        else:
+            seq.append(line.strip())
+
+    # yield the last sequence assuming there was at least one
+    if name is not None:
+        yield (name, ''.join(seq))
+
+def generate_fasta_windows(stream, read_length=100):
+    for name, sequence in read_fasta(stream):
+        for i in xrange(len(sequence)-read_length+1):
+            print('>{}_[{}:{})'.format(name.replace(' ', '_'), i, i+read_length ))
+            print(sequence[i:i+read_length])
+
+
+if __name__ == '__main__':
+    main()
